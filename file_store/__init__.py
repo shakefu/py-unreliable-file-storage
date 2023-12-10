4e97a5b3a2730ff29e3ec6810dfa34168e5b7b56
Jacob Alheid <shakefu@gmail.com>
2023-12-09 20:40:45 -0800

    fix(file_store): export class at package level

---
+++ file_store/__init__.py
@@ -0,0 +1,11 @@
+"""
+This package exports the FileStore class.
+
+The FileStore class provides unreliable block storage for multiple files.
+
+This is just at toy, and should never be used for anything ever.
+
+"""
+from .file_store import FileStore
+
+__all__ = ["FileStore"]
